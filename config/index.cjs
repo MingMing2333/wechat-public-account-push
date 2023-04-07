efa11b66294968e8056d706c9975ae04d65b61f3
MingMing2333 <107029516+MingMing2333@users.noreply.github.com>
2023-04-07 20:32:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,14 +6,12 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  
-  USE_PASSAGE: 'push-deer',
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx1c69ba3001dc52c0',
+  APP_ID: 'wx141e027b16883b21',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '9ca34a6416a5bf8a733c4d746158cbc7',
+  APP_SECRET: 'dabef085161d3f87b32a15cb0210c782',
 
   PROVINCE: '湖北',
   CITY: '武汉',
@@ -23,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21644TfEa31eOo7nyZXVyNbt5hTeHaxwF9UFnV',
+      id: 'oMMmL6A9_LBHe9Iyl32_TzOeJhM0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -59,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21644TfEa31eOo7nyZXVyNbt5hTeHaxwF9UFnV',
+      id: 'oMMmL6A9_LBHe9Iyl32_TzOeJhM0',
     }
   ],
 
