d2e6541901de988755fb8841152d3ca682f365ee
MingMing2333 <107029516+MingMing2333@users.noreply.github.com>
2023-04-08 09:27:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'Holly宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oMMmL6DTn_sXZXAQUTnkvX5FilA0',
+      id: 'oMMmL6A9_LBHe9Iyl32_TzOeJhM0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3OwWedFTZSaPbMvrzQ_AK2NoMCGKHfCGpMp630WSZpM',
+      useTemplateId: 'wKa5Evdu1YNQMyaqOqCwNYw5LFXFdo38Wg6D-EIPrMM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-22',
       festivals: [
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2021-06-27' }
+        { keyword: 'love_day', date: '2021-06-27' },
 //         // 结婚纪念日
-//         { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'leave_day', date: '2023-07-25' },
       ],
     },
   ],
