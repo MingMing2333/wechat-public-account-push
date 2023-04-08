fef7cc14727c21ff8e652e69072c966d7e0ac994
MingMing2333 <107029516+MingMing2333@users.noreply.github.com>
2023-04-08 09:51:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oMMmL6A9_LBHe9Iyl32_TzOeJhM0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'qEIlksqGXmSbdc0qk1Z-lL8lm5AVExFPrpL6ZnqjTTw',
+      useTemplateId: 'VuY3Mpefm9T7Ktqe2h3FVVlS0Wa1GqJDXBZcIQkql90',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-22',
       festivals: [
