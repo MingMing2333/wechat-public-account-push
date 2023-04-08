6b63a58ccf6c5d7d85cfe80951d8daf223fd62ba
MingMing2333 <107029516+MingMing2333@users.noreply.github.com>
2023-04-08 09:45:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: 'Holly宝贝',
+      name: '霍丽',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oMMmL6A9_LBHe9Iyl32_TzOeJhM0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wKa5Evdu1YNQMyaqOqCwNYw5LFXFdo38Wg6D-EIPrMM',
+      useTemplateId: 'qEIlksqGXmSbdc0qk1Z-lL8lm5AVExFPrpL6ZnqjTTw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-22',
       festivals: [
@@ -32,19 +32,22 @@ const USER_CONFIG = {
           type: '*生日', name: 'Holly宝贝', year: '2000', date: '03-18',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {
+//           type: '生日', name: 'Holly宝贝', year: '2000', date: '04-22',
+//         },
         {
-          type: '生日', name: 'Holly宝贝', year: '2000', date: '04-22',
+          type: '节日', name: '相识纪念日（飞机上相遇）', year: '2021', date: '06-07',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2021', date: '06-07',
+          type: '节日', name: '离汉回嘉鱼', year: '2023', date: '07-25',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2021-06-27' },
-//         // 结婚纪念日
-        { keyword: 'leave_day', date: '2023-07-25' },
+//         // 离汉日期
+//         { keyword: 'leave_day', date: '2023-07-25' },
       ],
     },
   ],
